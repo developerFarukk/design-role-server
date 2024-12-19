3851c3d4417150e362dd217db81b6d3d539a77c9
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-19 22:59:52 +0600

    blog validation Creat

--- src/app/modules/blog/blog.validation.ts
+++ src/app/modules/blog/blog.validation.ts
@@ -5,13 +5,14 @@ import { z } from "zod";
 // Create blog validation
 const createBlogValidation = z.object({
     body: z.object({
-        name: z.string({
-            invalid_type_error: 'Academic department must be string',
-            required_error: 'Name is required',
+        title: z.string({
+            required_error: 'Title is required',
         }),
-        academicFaculty: z.string({
-            invalid_type_error: 'Academic faculty must be string',
-            required_error: 'Faculty is required',
+        content: z.string({
+            required_error: 'content is required',
+        }),
+        author: z.string({
+            required_error: 'content is required',
         }),
     }),
 });
@@ -19,5 +20,4 @@ const createBlogValidation = z.object({
 
 export const AcademicDepartmentValidation = {
     createBlogValidation,
-    // updateAcademicDepartmentValidationSchema,
 };
