e34a12e47066bd2074e5c17537268bf0dc0e0af7
FARUK2059 <omarfaruk34552@gmail.com>
2024-12-22 11:00:31 +0600

    update user blocked function

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,6 +2,25 @@
 import { User } from "../user/user.model";
 
 const userBlockWithAdminFromDB = async (userId: string) => {
+
+    const user = await User.findById(userId);
+
+    // Check User Find 
+    if (!user) {
+        throw new Error('This user is not found !')
+    }
+
+    // Check User Already Blocked
+    if (user.isBlocked) {
+        throw new Error('User is already blocked!')
+    }
+
+    // Check User Admin
+    if (user.role === 'admin') {
+        throw new Error('This user is an admin and cannot be blocked')
+    }
+
+
     const result = await User.findOneAndUpdate(
         { _id: userId },
         { isBlocked: true },
@@ -9,8 +28,6 @@ const userBlockWithAdminFromDB = async (userId: string) => {
             new: true,
         },
     )
-    //    console.log(result);
-       
     return result;
 };
 
