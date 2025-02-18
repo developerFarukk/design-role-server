bbfb61b502e7e42c65c82c6a422687c2ccd840e3
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-19 00:35:43 +0600

    project interface setup

--- src/app/modules/blog/blog.service.ts
+++ src/app/modules/blog/blog.service.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 
 import { Tblog } from "./blog.interface";
 import { Blogs } from "./blog.model";
---
+++ src/app/modules/projects/project.interface.ts
@@ -0,0 +1,10 @@
+
+export type Tblog = {
+    _id?: string,
+    title: string;
+    descriptions: string;
+    livelink: string;
+    image?: string;
+    githubClient?: string
+    githubServer?: string
+};
