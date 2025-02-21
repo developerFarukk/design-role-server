e8392007c40d342a8988395e0d0bf748619e6487
FARUK2059 <omarfaruk34552@gmail.com>
2025-02-21 17:32:51 +0600

    deploy esu

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,7 @@ app.use(express.json())
 // app.use(cors());
 
 // app.use(cors({ origin: ['http://localhost:5001'] }));
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
-// app.use(cors({ origin: ['https://ridenest.vercel.app', 'http://localhost:5173'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://design-role-six.vercel.app'], credentials: true }));
 
 // application routes
 app.use('/api', router);
